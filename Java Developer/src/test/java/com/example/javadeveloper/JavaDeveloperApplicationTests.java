d40a4b3313ce07c82626510a5c5deda72b37e3cb
lukasz-cpu <lukasz.popek@hotmail.com>
2025-03-17 13:51:29 +0100

    initial code

---
+++ Java Developer/src/test/java/com/example/javadeveloper/JavaDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javadeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDeveloperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
